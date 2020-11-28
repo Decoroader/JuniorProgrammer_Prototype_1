e29204b5d2d74d2953c9372782910038f7e731e4
Decoroader <secbittree@gmail.com>
2020-11-28 22:36:41 +0200

    Lesson 1.4 - Step into the Driver's Seat 4
    4.Take control of the vehicle speed

--- Assets/Scripts/PlayerConroller.cs
+++ Assets/Scripts/PlayerConroller.cs
@@ -5,12 +5,14 @@ public class PlayerConroller : MonoBehaviour
     public float vehicleSpeed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
+    public float verticalInput;
 
     void FixedUpdate()
     {
         // Move the vahicle forward
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput =   Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-        transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed * verticalInput);
     }
 }
