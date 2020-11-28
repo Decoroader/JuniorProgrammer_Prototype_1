a244a50c442201873ce30e1c3b8d5a4ca187aaf8
Decoroader <secbittree@gmail.com>
2020-11-28 22:20:59 +0200

    Lesson 1.4 - Step into the Driver's Seat 2
    2.Allow the vehicle to move left/right

--- Assets/Scripts/PlayerConroller.cs
+++ Assets/Scripts/PlayerConroller.cs
@@ -3,11 +3,12 @@
 public class PlayerConroller : MonoBehaviour
 {
     public float vehicleSpeed = 5.0f;
-
+    public float turnSpeed;
 
     void FixedUpdate()
     {
         // Move the vahicle forward
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
         transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed);
     }
 }
