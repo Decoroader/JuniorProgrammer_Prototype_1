fb87ddfd4364829b9fe20e26c237e799d549a64f
Decoroader <secbittree@gmail.com>
2020-11-28 18:00:59 +0200

    Lesson 1.3 - High Speed Chase 4
    4.Add an offset to the camera position

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,7 @@ public class FollowPlayer : MonoBehaviour
     public GameObject playerViewer;
     void Update()
     {
-        transform.position = playerViewer.transform.position;
+        // offset the camera behind and right the player by adding to player's position
+        transform.position = playerViewer.transform.position + new Vector3(3.0f, 7.0f, -15.0f);
     }
 }
