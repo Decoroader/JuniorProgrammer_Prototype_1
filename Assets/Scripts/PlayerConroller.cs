81f8c018f006f5461f22b45cc2a13c0e2bb57337
Decoroader <secbittree@gmail.com>
2020-11-28 16:56:33 +0200

    Lesson 1.2 - Pedal to the Metal 3 and 4
    3.Add a comment in the Update() method, 4.Give the vehicle a forward motion

--- Assets/Scripts/PlayerConroller.cs
+++ Assets/Scripts/PlayerConroller.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayerConroller : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move the vahicle forward
+        transform.Translate(0f, 0f, 1f);
     }
 }
