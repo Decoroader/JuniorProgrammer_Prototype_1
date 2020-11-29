d565056c0624d005d3437f80303a844fd8eb5725
Decoroader <secbittree@gmail.com>
2020-11-29 10:57:05 +0200

    Challenge 1 6, 7
    6.The camera is in front of the plane 7.The camera is not following the plane

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -3,7 +3,13 @@
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(25.0f, 1.0f, 0);
+    private Vector3 rotateToPlane = new Vector3(0, 90.0f, 0);
+
+	private void Start()
+	{
+        transform.Rotate(rotateToPlane);
+    }
 
     void Update()
     {
