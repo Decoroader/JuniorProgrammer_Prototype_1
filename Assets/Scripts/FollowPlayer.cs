9f1035643ffa684e7f81140094a401d8a17cff9a
Decoroader <secbittree@gmail.com>
2021-01-30 19:56:57 +0200

    Lesson 6.1 - Project Optimization, 2
    2.Unity Event Functions

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,7 +4,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject playerViewer;
     [SerializeField] private Vector3 offset = new Vector3(3.0f, 7.0f, -15.0f);
-    void Update()
+    void LateUpdate()
     {
         // offset the camera behind and right the player by adding to player's position
         transform.position = playerViewer.transform.position + offset;
